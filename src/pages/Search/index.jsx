22d710238e52d18a977c9b6351e545630f3a4f0a
Matheus Gama dos Santos <mths.gm@gmail.com>
2021-06-21 20:55:25 -0300

    feat: Add search div styling and form

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,43 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Header from "../../components/Header";
-import { Wrapper, MainContent, SearchBar } from "./styles";
+import { Wrapper, MainContent, SearchBar, SearchResults, SearchResultDiv } from "./styles";
 
 import { BsSearch } from "react-icons/bs";
-import { GrOverview } from "react-icons/gr";
+import { MdPageview } from "react-icons/md";
 
 import lowOpacityChessboard from "../../assets/images/lowOpacityChessboard.png";
 
 const SearchPage = () => {
+  const [searchResults, setSearchResults] = useState([
+    {
+      title: "A vs B",
+      place: "Malásia",
+      numberOfMoves: 32,
+      date: "21/02/1998",
+      opening: "Scicilian",
+      score: "1-0"
+    },
+    {
+      title: "A vs B",
+      place: "Malásia",
+      numberOfMoves: 32,
+      date: "21/02/1998",
+      opening: "Scicilian",
+      score: "1-0"
+    }
+  ]);
+  const inputRef = useRef(null);
+
+  const handleSearch = () => {
+    console.info(inputRef.current?.value);
+  }
+
   return (
     <Wrapper>
       <Header />
       <MainContent>
         <SearchBar>
-          <input placeholder="Search for games here" />
-          <div id="Param-toggle"></div>
-          <button>
+          <input placeholder="Search for games here" ref={inputRef}/>
+          <button onClick={handleSearch}>
             <BsSearch size={20} />
           </button>
         </SearchBar>
 
-        <div id="Results-Container">
-          <div id="result">
-            <div id="general-info">
-              <h2 id="players">Content</h2>
-              <p id="place"></p>
-              <p id="moves"></p>
-              <p id="date"></p>
-            </div>
-            <div id="opening-and-result">
-              <p id="Opening"></p>
-              <h1 id="score">Content</h1>
-            </div>
-            <button id="visualize">
-              <GrOverview />
-            </button>
-          </div>
-        </div>
+        <SearchResults>
+          {
+            searchResults.map(result => (
+              <SearchResultDiv>
+                <div id="general-info">
+                  <h2 id="players">{result.title}</h2>
+                  <p id="place">{result.place}</p>
+                  <p id="moves">{result.numberOfMoves}</p>
+                  <p id="date">{result.date}</p>
+                </div>
+                <div id="opening-and-result">
+                  <p id="Opening">{result.opening}</p>
+                  <h1 id="score">{result.score}</h1>
+                </div>
+                <MdPageview size={50} />
+              </SearchResultDiv>
+              )
+            )
+          }
+          
+        </SearchResults>
 
         <img
           src={lowOpacityChessboard}
--- src/pages/Search/styles.js
+++ src/pages/Search/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  box-sizing: border-box;
   height: 100vh;
   width: 100%;
   overflow: hidden;
@@ -11,6 +12,7 @@ export const Wrapper = styled.div`
 `;
 
 export const MainContent = styled.div`
+  box-sizing: border-box;
   position: relative;
   overflow: hidden;
   display: flex;
@@ -29,11 +31,15 @@ export const MainContent = styled.div`
 `;
 
 export const SearchBar = styled.div`
+  box-sizing: border-box;
   width: 80%;
   max-width: 1200px;
   display: flex;
+  margin-bottom: 2rem;
+  z-index: 2;
 
   input {
+    box-sizing: border-box;
     width: 100%;
     background-color: #414141;
     color: #bbbbbb;
@@ -47,13 +53,85 @@ export const SearchBar = styled.div`
   }
 
   button {
+    box-sizing: border-box;
     background-color: #496b00;
     color: #fff;
+    cursor: pointer;
+    outline: none;
     border: none;
     padding: 15px 20px;
     border-radius: 0 10px 10px 0;
     display: flex;
     align-items: center;
     justify-content: center;
+    transition: 0.2s ease-in-out;
+
+    &:hover{
+      background-color: #6da100;
+      color: #e0e0e0;
+    }
+  }
+`;
+
+export const SearchResults = styled.div`
+  box-sizing: border-box;
+  width: 80%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
+`;
+
+export const SearchResultDiv = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  background-color: #2D2D2D;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.8rem 2.75rem;
+  font-family: "Roboto";
+
+  h1 {
+    font-weight: 700;
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    font-weight: 500;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    font-size: 0.875rem;
+    font-weight: 400;
+  }
+
+  p ~ p {
+    margin-top: 0.2rem;
+  }
+
+  #opening-and-result {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  svg {
+    margin-top: 0.5rem;
+    color: #F3F3F3;
+    cursor: pointer;
+    transition: 0.2s ease-in-out;
+
+    &:hover {
+      color: #8368ed;
+    }
+  }
+
+  & ~ & {
+    margin-top: 1rem;
   }
 `;
